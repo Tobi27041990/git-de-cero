a27ce69f4478e5e6c4e0b2a2c5ae2622de1e6030
Tobi27041990 <37884631+Tobi27041990@users.noreply.github.com>
2018-08-29 21:45:31 -0300

    s

--- src/paquete/MainClass.java
+++ src/paquete/MainClass.java
@@ -4,7 +4,8 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Chau Mundo!!");
-		System.out.println("Como est‡s hoy?");
+		System.out.println("Como est√†s hoy?");
+		System.out.println("Bien y vos?");
 	}
 
 }
