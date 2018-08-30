43b3da189955178dc11432e2615a3aaa8a29192d
Tobias <tobias.wild2704@gmail.com>
2018-08-29 21:24:58 -0300

    Aceptamos los cambiamos

--- src/paquete/MainClass.java
+++ src/paquete/MainClass.java
@@ -3,7 +3,8 @@ package paquete;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo!!");
+		System.out.println("Chau Mundo!!");
+		System.out.println("Como estàs hoy?");
 	}
 
 }
